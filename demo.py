f4191da2a12872620f0440b89ded016388dcc4fc
TDI <Risingabhi@gmail.com>
2023-08-03 20:59:29 +0530

    Update demo.py
    updated

--- demo.py
+++ demo.py
@@ -34,7 +34,7 @@ import PyPDF2
 st.title("File Upload Example")
 
 # Add a file uploader widget with a label
-uploaded_file = st.file_uploader("Choose a file", type=["pdf", "txt"])
+uploaded_file = st.file_uploader("Choose a file", type=["pdf"])
 
 # Check if a file was uploaded
 if uploaded_file is not None:
@@ -47,20 +47,13 @@ if uploaded_file is not None:
 
     # Read and display the content of the uploaded file
     st.write("Content:")
-    if uploaded_file.type == "application/pdf":
-        # For PDF files, read and display the text content
-        pdf_reader = PyPDF2.PdfFileReader(uploaded_file)
-        num_pages = pdf_reader.getNumPages()
-        for page_num in range(num_pages):
-            page = pdf_reader.getPage(page_num)
-            st.write(page.extract_text())
-    elif uploaded_file.type == "text/plain":
-        # For TXT files, read and display the text content
-        text_content = uploaded_file.read().decode("utf-8")
-        st.write(text_content)
+    pdf_reader = PyPDF2.PdfReader(uploaded_file)
+    num_pages = len(pdf_reader.pages)
+    for page_num in range(num_pages):
+        page = pdf_reader.pages[page_num]
+        st.write(page.extract_text())
 
     # Optionally, you can save the file to a temporary location or perform further processing.
     # For example, to save the file with the original filename:
     # with open(uploaded_file.name, "wb") as f:
     #     f.write(uploaded_file.getbuffer())
-
