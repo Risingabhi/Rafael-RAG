323442ceea18490d99fdcf1831b650e402326aab
TDI <Risingabhi@gmail.com>
2023-08-03 16:17:23 +0530

    main file uploaded here
    main file 3/8/23

---
+++ demo.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from PIL import Image
+
+
+
+#choose option at top
+#1 Professor's Assistant
+#2 Student's Assistant
+
+
+image = Image.open('C:/Users/Risin/Desktop/infolab/infolab.png')
+
+st.image(image, caption='Introducing RAG powered by Ai')
+
+st.checkbox("Professor's Assistant")
+st.checkbox("Student's Assistant")
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+st.button('Upload Doc')
+title = st.text_input('Your Query Here', 'Write your query')
+st.write('You asked Ai', title)
+st.button('send')
